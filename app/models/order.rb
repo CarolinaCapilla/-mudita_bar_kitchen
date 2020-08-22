88d1560678c3286cb69e0d9bbfe6e8c656926b0a
CarolinaCapilla <carolina.capilla@gmail.com>
2020-08-22 11:25:18 +0100

    fixed typos

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :items, dependant: :destroy
+  has_many :items, dependent: :destroy
 
   validates :devlivery_address, presence: true
 end
