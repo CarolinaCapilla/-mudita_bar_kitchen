16cc4ff332c12fc6766b1dd46f812e7841ac8ac4
CarolinaCapilla <carolina.capilla@gmail.com>
2020-08-25 21:13:17 +0100

    started with form for new,edit item

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,51 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: %i[show edit update destroy]
+  # skip_before_action :authenticate_user!, only: %i[show index]
+
+  def show
+    if current_user == (user.chef == true)
+      @items = Items.where(user: user)
+    else
+      @items = Item.all
+    end
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to item_path(@item), notice: "Item successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: "Item successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path, notice: "Item successfully destroyed."
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:dish, :description, :category, :price, :allergen)
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,23 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  # validates :dish, :description, :category, :price, :allergen, presence: true
+  CATEGORIES = ["Starters", "Mains", "Desserts", "Sharing", "Platters", "Snacks", "Specials", "Drinks", "Soft Drinks",
+                "Hot Drinks", "Alcoholic Drinks"]
+  ALLERGENS = ["Celery", "Cereals containing gluten", "Crustaceans", "Eggs", "Fish", "Lupin", "Milk", "Molluscs",
+               "Mustard", "Tree nuts", "Peanuts", "Sesame seeds", "Soybeans", "Sulphur dioxide and sulphites"]
+
+  validates :dish, :description, :category, :price, :allergen, :ingredient, presence: true
   # validates :description, length: { minimum: 300 }
-  # validates :category, inclusion: { in: ["Starters", "Mains", "Desserts", "Sharing", "Platters", "Snacks",
-  #                                        "Specials",
-  #                                        "Drinks",
-  #                                        "Soft Drinks",
-  #                                        "Hot Drinks",
-  #                                        "Alcoholic Drinks"] }
-  # validates :allergen, inclusion: { in: ["Celery", "Cereals containing gluten", "Crustaceans", "Eggs", "Fish",
-  #                                        "Lupin",
-  #                                        "Milk",
-  #                                        "Molluscs",
-  #                                        "Mustard",
-  #                                        "Tree nuts",
-  #                                        "Peanuts",
-  #                                        "Sesame seeds",
-  #                                        "Soybeans",
-  #                                        "Sulphur dioxide and sulphites"] }
-  # validates :price, numericality: true
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :allergen, inclusion: { in: ALLERGENS  }
+  validates :price, numericality: true
 end
