9ef43c9a58c27323b8134ef439b8bd6df49b5243
yazeed almusaybih <yazeedalmusaybih@gmail.com>
2020-08-25 19:48:05 +0100

    done from kichent controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :kitchens do
-  #   resources :stations, only: [:create, :new, :edit, :update]
-  #   resources :bookings, only: [:create, :new, :edit, :update]
-  # end
-  # resources :stations, only: [:show, :index, :destroy]
-  # resources :bookings, only: [:show, :index, :destroy]
-end
-=======
 
   resources :items, except: [:index] do
     resources :orders
@@ -26,5 +16,4 @@ end
   resources :stations do
     resources :bookings
   end
->>>>>>> a0a583e8868492949f07c887a7615574345c5c30
 end
