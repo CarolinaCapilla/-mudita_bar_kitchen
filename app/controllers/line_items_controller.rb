c4f39d0ff6987251a1412d482f26628e13b39556
CarolinaCapilla <carolina.capilla@gmail.com>
2020-09-12 23:16:36 +0100

    removed unnecessary files

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -20,7 +20,6 @@ class LineItemsController < ApplicationController
     set_cart
     item = Item.find(params[:item_id])
     @line_item = @cart.add_item(item)
-    raise
     if @line_item.save
       redirect_to cart_path(@cart), alert: 'Item added to cart.'
     else
