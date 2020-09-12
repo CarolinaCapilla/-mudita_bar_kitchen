d4df5cce0b70ef0f20622f23e3d19a8b0e906ace
CarolinaCapilla <carolina.capilla@gmail.com>
2020-09-12 16:59:20 +0100

    starting views for line_items and cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,7 +12,7 @@ class Cart < ApplicationRecord
   end
 
   def total_price
-    line_items.to_a.sum(&:item.total_price)
-    # line_items.to_a.sum { |item| item.total_price }
+    line_items.to_a.sum(&:total_price)
+    # line_items.to_a.sum 
   end
 end
