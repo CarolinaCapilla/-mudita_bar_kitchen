64da9418d5f25b180a13d9ad330fa108db120f19
CarolinaCapilla <carolina.capilla@gmail.com>
2020-09-15 22:14:31 +0100

    fixed cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_105601) do
+ActiveRecord::Schema.define(version: 2020_09_15_210807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 2020_09_12_105601) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "status", default: false
     t.index ["user_id"], name: "index_kitchens_on_user_id"
   end
 
