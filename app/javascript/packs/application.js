e5aec78c00038da72f0c7f54644a918e530454ff
CarolinaCapilla <carolina.capilla@gmail.com>
2020-09-17 22:35:12 +0100

    removed useless stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("sweetalert2")
-require ("sweet-alert2-rails")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -26,8 +24,6 @@ require ("sweet-alert2-rails")
 
 // External imports
 import "bootstrap";
-import swal from 'sweetalert';
-
 
 // Internal imports, e.g:
 import { allergenToggle } from '../components/allergen_choices'
