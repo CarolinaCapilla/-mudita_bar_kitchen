132330c08b692264afb0f879a3fafb9811a59f5d
CarolinaCapilla <carolina.capilla@gmail.com>
2020-09-22 20:09:57 +0100

    fixed modal js

--- app/javascript/components/payment_modal.js
+++ app/javascript/components/payment_modal.js
@@ -3,14 +3,16 @@ import Swal from 'sweetalert2'
 const paymentButton = document.querySelector('.payment-modal')
 
 const paymentButtonPopOut = () => {
-  paymentButton.addEventListener('click', (event) => {
-    Swal.fire({
-      title: 'Card details confirmed',
-      text: 'Payment completed',
-      icon: 'success',
-      confirmButtonText: 'OK'
+  if (paymentButton != null){
+    paymentButton.addEventListener('click', (event) => {
+      Swal.fire({
+        title: 'Card details confirmed',
+        text: 'Payment completed',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      });
     });
-  });
+  }
 }
 
 export { paymentButtonPopOut }
