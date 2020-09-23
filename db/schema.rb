7911d45e83b198573df61111ff9bcfd135f8228e
CarolinaCapilla <carolina.capilla@gmail.com>
2020-09-23 19:00:29 +0100

    fixed edit/delete from items and kitchens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_155518) do
+ActiveRecord::Schema.define(version: 2020_09_23_143637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,7 +96,9 @@ ActiveRecord::Schema.define(version: 2020_09_19_155518) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "cart_id"
+    t.bigint "item_id"
     t.index ["cart_id"], name: "index_orders_on_cart_id"
+    t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -148,6 +150,7 @@ ActiveRecord::Schema.define(version: 2020_09_19_155518) do
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "items"
   add_foreign_key "orders", "carts"
+  add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
   add_foreign_key "reviews", "kitchens"
   add_foreign_key "reviews", "users"
